abb720d5be8965473d97a511a5d833b6df5d25d7
PY-TU-01-24 <d-a-n-e@email.cz>
2024-04-02 18:49:46 +0200

    class

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
 class Student:
-    age = 20
-    name = "Pavel"
+    sepc = "devOps"
+    def __init__(self, name, year):
+        self.name =  name
+        self.age = 2024 - year
 
-student1 = Student()
+student1 = Student("Pavel", 2000)
 print(student1.name)
+print(student1.age)
 
-student2 = Student()
+student2 = Student("Adam", 1999)
+print(student2.name)
 print(student2.age)
-
-print(student2)
