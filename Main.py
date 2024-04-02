d722a7935d6227a869b496077b23e7f79f835613
PY-TU-01-24 <d-a-n-e@email.cz>
2024-04-02 20:18:28 +0200

    class2

--- Main.py
+++ Main.py
@@ -1,13 +1,27 @@
 class Student:
     sepc = "devOps"
-    def __init__(self, name, year):
+    def __init__(self, name, age):
         self.name =  name
-        self.age = 2024 - year
+        self.age = age
+    def showMsg(self):
+        print(f"moje jmeno je {self.name} a muj vek je {self.age}")
 
-student1 = Student("Pavel", 2000)
-print(student1.name)
-print(student1.age)
+    def sayHi(self, greeting):
+        print(f"{greeting}, ja jsem{self.name}")
 
-student2 = Student("Adam", 1999)
-print(student2.name)
-print(student2.age)
+student = Student(" Adam", 55)
+student1 = Student(" Osel", 55)
+student.showMsg()
+
+student1.sayHi("ahoj")
+student1.sayHi("dobrý denk")
+
+
+
+# student1 = Student("Pavel", 2000)
+# print(student1.name)
+# print(student1.age)
+#
+# student2 = Student("Adam", 1999)
+# print(student2.name)
+# print(student2.age)
